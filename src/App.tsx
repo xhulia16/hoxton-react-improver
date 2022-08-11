f6f83e1174316fa158a63a0bc8e5ba766e9a38e3
XHULIA PRISHKA <xh.prishka@gmail.com>
2022-08-11 19:55:07 +0200

    commit fetch patch

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,87 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import { Todo } from './types'
-
+import { useEffect, useState } from "react";
+import "./App.css";
+import { Todo } from "./types";
 
 function App() {
-const [todos, setTodos]=useState<Todo[]>([])
+  const [todos, setTodos] = useState<Todo[]>([]);
 
-useEffect(()=>{
-  fetch('http://localhost:4000/todos')
-  .then(resp=>resp.json())
-  .then(todosFromServer=> setTodos(todosFromServer))
-}, [])
+  useEffect(() => {
+    fetch("http://localhost:4000/todos")
+      .then((resp) => resp.json())
+      .then((todosFromServer) => setTodos(todosFromServer));
+  }, []);
+
+   function toggleTask(item: Todo){
+    const todosCopy=structuredClone(todos)
+                let match=todosCopy.find(todo=>todo.id===item.id)
+              console.log(match)
+              match.finished=!item.finished
+   }
+
 
   return (
     <div className="App">
-    
       <h1>List of todos:</h1>
-      
-     <main>
-     <ul>
-      {todos.map(item=>(
-        <li key={item.id}>
-          <h3>{item.content}</h3>
-        </li>
-      ))}
-     </ul>
 
-<form onSubmit={(event)=>{
-event.preventDefault()
-fetch('http://localhost:4000/todos',{
-  method: "Post",
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({
-    content: event.target.todo.value,
-    finished: false
-  })
-})
-let todo={
-  content: event.target.todo.value,
-  finished: false
-}
 
-setTodos([...todos, todo])
-event.target.reset()
-}}>
-  <input type="text" name="todo" placeholder='Add a todo...'></input>
-  <button> submit </button>
-</form>
-     </main>
+      <main>
+        <ul>
+          {todos.map((item) => (
+            <li
+              key={item.id}
+              onClick={() => {
+
+                fetch(`http://localhost:4000/todos/${item.id}`, {
+                  method: "PATCH",
+                  body: JSON.stringify({
+                    finished: !item.finished,
+                  }),
+                  headers: {
+                    "Content-type": `application/json; charset=UTF-8`,
+                  },
+                })
+                .then((response) => response.json())
+                toggleTask(item)
+                
+              }
+            }
+
+
+              
+            >
+              <h3>{item.content}</h3>
+            </li>
+          ))}
+        </ul>
+
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+            fetch("http://localhost:4000/todos", {
+              method: "Post",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                content: event.target.todo.value,
+                finished: false,
+              }),
+            });
+            let todo = {
+              content: event.target.todo.value,
+              finished: false,
+            };
+
+            setTodos([...todos, todo]);
+            event.target.reset();
+          }}
+        >
+          <input type="text" name="todo" placeholder="Add a todo..."></input>
+          <button> submit </button>
+        </form>
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
