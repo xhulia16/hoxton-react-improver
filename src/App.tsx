8a80c2546af10e64af51658ac0a0f7e14e8fce38
XHULIA PRISHKA <xh.prishka@gmail.com>
2022-08-12 14:22:10 +0200

    search input

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Todo } from "./types";
 
 function App() {
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
     fetch("http://localhost:4000/todos")
@@ -11,55 +12,76 @@ function App() {
       .then((todosFromServer) => setTodos(todosFromServer));
   }, []);
 
-   function toggleTask(item: Todo){
-    const todosCopy=structuredClone(todos)
-                let match=todosCopy.find(todo=>todo.id===item.id)
-              match.finished=!item.finished
-              setTodos(todosCopy)
-   }
+  function toggleTask(item: Todo) {
+    const todosCopy = structuredClone(todos);
+    let match = todosCopy.find((todo) => todo.id === item.id);
+    match.finished = !item.finished;
+    setTodos(todosCopy);
+  }
 
-   function deleteTask(item: Todo){
-    let filteredTodos=todos.filter(todo=> todo.id !== item.id)
-    setTodos(filteredTodos)
-   }
+  function deleteTask(item: Todo) {
+    let filteredTodos = todos.filter((todo) => todo.id !== item.id);
+    setTodos(filteredTodos);
+  }
 
+  function searchTodo() {
+    let searchTodos = todos.filter((todo) => todo.content.toLowerCase().includes(query.toLowerCase()));
+    return(searchTodos)
+  }
 
   return (
     <div className="App">
+      <header>
+     <form>
+        <input
+          type="text"
+          placeholder="Search a todo..."
+          onChange={(event) => {
+            setQuery(event.target.value)
+            console.log(`query: ${query}`)
+            searchTodo()
+          }}
+        ></input>
+      </form>
+      </header>
+
       <h1>List of todos:</h1>
 
       <main>
         <ul>
-          {todos.map((item) => (
-            <li className={item.finished ? 'finished' : 'not-finished'}
-              key={item.id}>
-              <h3 
-               onClick={() => {
+          {searchTodo().map((item) => (
+            <li
+              className={item.finished ? "finished" : "not-finished"}
+              key={item.id}
+            >
+              <h3
+                onClick={() => {
+                  fetch(`http://localhost:4000/todos/${item.id}`, {
+                    method: "PATCH",
+                    body: JSON.stringify({
+                      finished: !item.finished,
+                    }),
+                    headers: {
+                      "Content-type": `application/json; charset=UTF-8`,
+                    },
+                  }).then((response) => response.json());
+                  toggleTask(item);
+                }}
+              >
+                {item.content}
+              </h3>
 
-                fetch(`http://localhost:4000/todos/${item.id}`, {
-                  method: "PATCH",
-                  body: JSON.stringify({
-                    finished: !item.finished,
-                  }),
-                  headers: {
-                    "Content-type": `application/json; charset=UTF-8`,
-                  },
-                })
-                .then((response) => response.json())
-                toggleTask(item)
-                
-              }
-            }>{item.content}</h3>
-              
-              <button onClick={()=>{
-                fetch(`http://localhost:4000/todos/${item.id}`,{
-                  method: 'DELETE',
-                })
-                .then(resp => resp.json())
-              
-                deleteTask(item)
-              }}>
-                delete</button>
+              <button
+                onClick={() => {
+                  fetch(`http://localhost:4000/todos/${item.id}`, {
+                    method: "DELETE",
+                  }).then((resp) => resp.json());
+
+                  deleteTask(item);
+                }}
+              >
+                delete
+              </button>
             </li>
           ))}
         </ul>
@@ -78,6 +100,7 @@ function App() {
               }),
             });
             let todo = {
+              id: Math.random(),
               content: event.target.todo.value,
               finished: false,
             };
